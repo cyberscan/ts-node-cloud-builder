2012cab413a5b1345ee3751279fcc4c4815bfbc4
pauldreyer <paul.dreyer@dgc.org>
2022-01-10 16:34:33 +0100

    fix slicing depth to 2

--- lerna-resolver/index.js
+++ lerna-resolver/index.js
@@ -93,7 +93,7 @@ function processPackage(package, destinationFolder) {
     all: true, // include files & directories begining with a dot
     follow: true, // dereference symlinks
     error: true, // throw error if nothing is copied
-    up: 4, // slice 4 dir from source destination (copyfiles fix)
+    up: 2, // slice 2 dir from source destination (copyfiles fix)
   };
 
   // files to copy
